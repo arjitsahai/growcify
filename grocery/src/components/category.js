4349da43e8f0edeadc7c15df20b8fe28c707fe95
arjitsahai <62187739+arjitsahai@users.noreply.github.com>
2021-01-13 17:51:00 +0530

    submit

--- grocery/src/components/category.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Category = ({photos, isActive, name, parent}) => {
-    <div className="category">
-        <img src ={photos} alt= {name} />
-    </div>
-}
-
-export default Category;
